c59f8f2d85a9d58b16bad845da6ac1bc97da8e9d
Wolfgang Hoenig <whoenig@usc.edu>
2017-12-15 14:53:32 -0800

    add genTrajectory

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # uav_trajectories
-Helper scripts and programs for trajectories
+Helper scripts and programs for trajectories to be used on UAVs
+
+# Requirements
+
+Parts of this software are based on [mav_trajectory_generation]: https://github.com/ethz-asl/mav_trajectory_generation, a software package developed at ETH Zurich, implementing a trajectory optimization approach developed at MIT.
+When using this work academically, follow their instructions on how to cite their work.
+We use the provided library in that package, but do not require ROS for execution.
+
+# Building
+
+Tested on Ubuntu 16.04. Install additional dependencies using:
+
+```
+sudo apt install libnlopt-dev libgoogle-glog-dev
+```
+
+Clone and build this repository:
+
+```
+git clone --recursive https://github.com/whoenig/uav_trajectories.git
+mkdir build
+cd build
+cmake ..
+make
+```
+
+## Polynomial Trajectories
+
+### Generate Trajectory given waypoints
+
+This program takes a sequence of waypoints and dynamic quadrotor limits as inputs, and produces a smooth trajectory (with 0 derivatives at the beginning and end) that can be executed safely.
+
+Example:
+
+```
+./genTrajectory -i ../examples/waypoints1.csv --v_max 1.0 --a_max 1.0 -o traj1.csv
+```
---
+++ src/genTrajectory.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <fstream>
+
+#include <boost/program_options.hpp>
+
+#include <mav_trajectory_generation/polynomial_optimization_linear.h>
+#include <mav_trajectory_generation/polynomial_optimization_nonlinear.h>
+
+
+using namespace Eigen;
+
+// see https://stackoverflow.com/questions/34247057/how-to-read-csv-file-and-assign-to-eigen-matrix
+template<typename M>
+M load_csv (const std::string & path) {
+  std::ifstream indata;
+  indata.open(path);
+  std::string line;
+  std::vector<double> values;
+  uint rows = 0;
+  while (std::getline(indata, line)) {
+    std::stringstream lineStream(line);
+    std::string cell;
+    while (std::getline(lineStream, cell, ',')) {
+      values.push_back(std::stod(cell));
+    }
+    ++rows;
+  }
+  return Map<const Matrix<typename M::Scalar, M::RowsAtCompileTime, M::ColsAtCompileTime, RowMajor>>(values.data(), rows, values.size()/rows);
+}
+
+int main(int argc, char **argv)
+{
+  std::string inputFile;
+  std::string outputFile;
+  double v_max;
+  double a_max;
+
+  namespace po = boost::program_options;
+
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help", "produce help message")
+    ("input,i", po::value<std::string>(&inputFile)->required(), "path to list of waypoints")
+    ("output,o", po::value<std::string>(&outputFile)->required(), "path to output files")
+    ("v_max", po::value<double>(&v_max)->default_value(1.0), "maximum velocity [m/s]")
+    ("a_max", po::value<double>(&a_max)->default_value(1.0), "maximum velocity [m/s^2]")
+  ;
+
+  try
+  {
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+      std::cout << desc << "\n";
+      return 0;
+    }
+  }
+  catch(po::error& e)
+  {
+    std::cerr << e.what() << std::endl << std::endl;
+    std::cerr << desc << std::endl;
+    return 1;
+  }
+
+
+  mav_trajectory_generation::Vertex::Vector vertices;
+  const int dimension = 3;
+  const int derivative_to_optimize = mav_trajectory_generation::derivative_order::JERK;
+
+  // create vertices with their constraints
+  auto input = load_csv<Matrix<double, Dynamic, 3> >(inputFile);
+
+  for (int row = 0; row < input.rows(); ++row) {
+    // std::cout << input.row(row) << std::endl;
+    mav_trajectory_generation::Vertex vertex(dimension);
+    if (row == 0 || row == input.rows() - 1) {
+      vertex.makeStartOrEnd(input.row(row), derivative_to_optimize);
+    } else {
+      vertex.addConstraint(mav_trajectory_generation::derivative_order::POSITION, input.row(row));
+    }
+    vertices.push_back(vertex);
+  }
+
+  // compute segment times
+  std::vector<double> segment_times;
+  const double magic_fabian_constant = 6.5; // A tuning parameter.
+  segment_times = estimateSegmentTimes(vertices, v_max, a_max, magic_fabian_constant);
+
+  // solve
+  const int N = 8;
+  mav_trajectory_generation::Segment::Vector segments;
+
+#if SOLVE_LINEAR
+  mav_trajectory_generation::PolynomialOptimization<N> opt(dimension);
+  opt.setupFromVertices(vertices, segment_times, derivative_to_optimize);
+  opt.solveLinear();
+
+  // Obtain the polynomial segments.
+  opt.getSegments(&segments);
+#else
+  mav_trajectory_generation::NonlinearOptimizationParameters parameters;
+  parameters.max_iterations = 1000;
+  parameters.f_rel = 0.05;
+  parameters.x_rel = 0.1;
+  parameters.time_penalty = 500.0;
+  parameters.initial_stepsize_rel = 0.1;
+  parameters.inequality_constraint_tolerance = 0.1;
+
+  mav_trajectory_generation::PolynomialOptimizationNonLinear<N> opt(dimension, parameters, false);
+  opt.setupFromVertices(vertices, segment_times, derivative_to_optimize);
+  opt.addMaximumMagnitudeConstraint(mav_trajectory_generation::derivative_order::VELOCITY, v_max);
+  opt.addMaximumMagnitudeConstraint(mav_trajectory_generation::derivative_order::ACCELERATION, a_max);
+  opt.optimize();
+
+  // Obtain the polynomial segments.
+  opt.getPolynomialOptimizationRef().getSegments(&segments);
+#endif
+
+  std::ofstream output(outputFile);
+
+  output << "Duration,x^0,x^1,x^2,x^3,x^4,x^5,x^6,x^7,y^0,y^1,y^2,y^3,y^4,y^5,y^6,y^7,z^0,z^1,z^2,z^3,z^4,z^5,z^6,z^7,yaw^0,yaw^1,yaw^2,yaw^3,yaw^4,yaw^5,yaw^6,yaw^7" << std::endl;
+  Eigen::IOFormat csv_fmt(Eigen::StreamPrecision, Eigen::DontAlignCols, ",", ",", "", "", "", "");
+
+  for (const auto& segment : segments) {
+    output << segment.getTime() << ",";
+    for (const auto& polynomial : segment.getPolynomialsRef()) {
+      Eigen::VectorXd coefficients = polynomial.getCoefficients();
+      output << coefficients.format(csv_fmt) << ",";
+    }
+    output << "0,0,0,0,0,0,0,0" << std::endl;
+  }
+
+  return 0;
+}
