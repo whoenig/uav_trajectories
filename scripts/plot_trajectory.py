b54dc9a48a4963495645530ec30d0f2e7d053e7e
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2022-07-18 19:33:55 +0200

    added two methods to check for mistakes

--- scripts/plot_trajectory.py
+++ scripts/plot_trajectory.py
@@ -11,37 +11,88 @@ import uav_trajectory
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("trajectory", type=str, help="CSV file containing trajectory")
+  parser.add_argument("--mass", type=float, help="mass of the UAV")
   parser.add_argument("--stretchtime", type=float, help="stretch time factor (smaller means faster)")
   args = parser.parse_args()
 
   traj = uav_trajectory.Trajectory()
   traj.loadcsv(args.trajectory)
 
+  if args.mass:
+    traj1 = uav_trajectory.Trajectory()
+    traj1.loadcsv(args.trajectory)
+    traj2 = uav_trajectory.Trajectory()
+    traj2.loadcsv(args.trajectory)
+    traj1.addmass(args.mass)
+    traj2.addmass(args.mass)
+    
   if args.stretchtime:
     traj.stretchtime(args.stretchtime)
-
+    traj1.stretchtime(args.stretchtime)
+    traj2.stretchtime(args.stretchtime)
+    
   ts = np.arange(0, traj.duration, 0.01)
-  evals = np.empty((len(ts), 15))
+  evals = np.empty((len(ts), 21))
   for t, i in zip(ts, range(0, len(ts))):
     e = traj.eval(t)
-    evals[i, 0:3]  = e.pos
-    evals[i, 3:6]  = e.vel
-    evals[i, 6:9]  = e.acc
-    evals[i, 9:12] = e.omega
-    evals[i, 12]   = e.yaw
-    evals[i, 13]   = e.roll
-    evals[i, 14]   = e.pitch
+    evals[i, 0:3]   = e.pos
+    evals[i, 3:6]   = e.vel
+    evals[i, 6:9]   = e.acc
+    evals[i, 9:12]  = e.omega
+    evals[i, 12]    = e.yaw
+    evals[i, 13]    = e.roll
+    evals[i, 14]    = e.pitch
+    evals[i, 15:18] = e.jerk
+    evals[i, 18::]  = e.snap
 
+  if args.mass:
+    evals1 = np.empty((len(ts), 21))
+    evals2 = np.empty((len(ts), 21))
+    for t, i in zip(ts, range(0, len(ts))):
+      e1 = traj1.eval1(t)
+      e2 = traj2.eval2(t)
+      evals1[i, 0:3]  = e1.pos
+      evals1[i, 3:6]  = e1.vel
+      evals1[i, 6:9]  = e1.acc
+      evals1[i, 9:12] = e1.omega
+      evals1[i, 12]   = e1.yaw
+      evals1[i, 13]   = e1.roll
+      evals1[i, 14]   = e1.pitch
+      evals1[i, 15:18] = e1.jerk
+      evals1[i, 18::]  = e1.snap
+      evals2[i, 0:3]  = e2.pos
+      evals2[i, 3:6]  = e2.vel
+      evals2[i, 6:9]  = e2.acc
+      evals2[i, 9:12] = e2.omega
+      evals2[i, 12]   = e2.yaw
+      evals2[i, 13]   = e2.roll
+      evals2[i, 14]   = e2.pitch
+      evals2[i, 15:18] = e2.jerk
+      evals2[i, 18::]  = e2.snap
+    velocity1 = np.linalg.norm(evals1[:,3:6], axis=1)
+    velocity2 = np.linalg.norm(evals2[:,3:6], axis=1)
+    acceleration1 = np.linalg.norm(evals1[:,6:9], axis=1)
+    acceleration2 = np.linalg.norm(evals2[:,6:9], axis=1)
+    omega1 = np.linalg.norm(evals1[:,9:12], axis=1)
+    omega2 = np.linalg.norm(evals2[:,9:12], axis=1)
+    
   velocity = np.linalg.norm(evals[:,3:6], axis=1)
   acceleration = np.linalg.norm(evals[:,6:9], axis=1)
   omega = np.linalg.norm(evals[:,9:12], axis=1)
 
   # print stats
-  print("max speed (m/s): ", np.max(velocity))
-  print("max acceleration (m/s^2): ", np.max(acceleration))
-  print("max omega (rad/s): ", np.max(omega))
-  print("max roll (deg): ", np.max(np.degrees(evals[:,13])))
-  print("max pitch (deg): ", np.max(np.degrees(evals[:,14])))
+  if args.mass:
+    print("max speed (m/s): (W., Fr., Scr.)", np.max(velocity), np.max(velocity1), np.max(velocity2))
+    print("max acceleration (m/s^2): (W., Fr., Scr.) ", np.max(acceleration), np.max(acceleration1), np.max(acceleration2))
+    print("max omega (rad/s): (W., Fr., Scr.) ", np.max(omega), np.max(omega1), np.max(omega2))
+    print("max roll (deg): (W., Fr., Scr.) ", np.max(np.degrees(evals[:,13])), np.max(np.degrees(evals1[:,13])), np.max(np.degrees(evals2[:,13])))
+    print("max pitch (deg): (W., Fr., Scr.) ", np.max(np.degrees(evals[:,15])), np.max(np.degrees(evals1[:,15])), np.max(np.degrees(evals2[:,14])))
+  else:
+    print("max speed (m/s): ", np.max(velocity))
+    print("max acceleration (m/s^2): ", np.max(acceleration))
+    print("max omega (rad/s): ", np.max(omega))
+    print("max roll (deg): ", np.max(np.degrees(evals[:,13])))
+    print("max pitch (deg): ", np.max(np.degrees(evals[:,14])))
 
   # Create 3x1 sub plots
   gs = gridspec.GridSpec(6, 1)
@@ -60,19 +111,37 @@ if __name__ == "__main__":
 
   ax = plt.subplot(gs[4, 0]) # row 4
   ax.plot(ts, omega)
+  if args.mass:
+    ax.plot(ts, omega1)
+    ax.plot(ts, omega2)
   ax.set_ylabel("omega [rad/s]")
 
   ax = plt.subplot(gs[5, 0]) # row 5
   ax.plot(ts, np.degrees(evals[:,12]))
   ax.set_ylabel("yaw [deg]")
 
-  # ax = plt.subplot(gs[6, 0]) # row 5
-  # ax.plot(ts, np.degrees(evals[:,13]))
-  # ax.set_ylabel("roll [deg]")
+  if args.mass:
+    gs = gridspec.GridSpec(3, 1)
+    fig1 = plt.figure()
+    plt.legend('omega_i')
 
-  # ax = plt.subplot(gs[7, 0]) # row 5
-  # ax.plot(ts, np.degrees(evals[:,14]))
-  # ax.set_ylabel("pitch [deg]")
+    ax = plt.subplot(gs[0, 0])
+    ax.plot(ts, evals[:,9])
+    ax.plot(ts, evals1[:,9])
+    ax.plot(ts, evals2[:,9])
+    ax.set_ylabel('omega_x')
+    
+    ax = plt.subplot(gs[1, 0])
+    ax.plot(ts, evals[:,10])
+    ax.plot(ts, evals1[:,10])
+    ax.plot(ts, evals2[:,10])
+    ax.set_ylabel('omega_y')
+
+    ax = plt.subplot(gs[2, 0])
+    ax.plot(ts, evals[:,11])
+    ax.plot(ts, evals1[:,11])
+    ax.plot(ts, evals2[:,11])
+    ax.set_ylabel('omega_z')
 
   plt.show()
 
--- scripts/uav_trajectory.py
+++ scripts/uav_trajectory.py
@@ -6,6 +6,8 @@ def normalize(v):
   assert norm > 0
   return v / norm
 
+def norm(v):
+  return np.linalg.norm(v)
 
 class Polynomial:
   def __init__(self, p):
@@ -40,8 +42,8 @@ class TrajectoryOutput:
     self.yaw = None   # yaw angle [rad]
     self.roll = None  # required roll angle [rad]
     self.pitch = None # required pitch angle [rad]
-
-
+    self.jerk = None
+    self.snap = None
 # 4d single polynomial piece for x-y-z-yaw, includes duration.
 class Polynomial4D:
   def __init__(self, duration, px, py, pz, pyaw):
@@ -85,9 +87,13 @@ class Polynomial4D:
     derivative2 = derivative.derivative()
     result.acc = np.array([derivative2.px.eval(t), derivative2.py.eval(t), derivative2.pz.eval(t)])
 
-    # 3rd derivative
+   # 3rd derivative
     derivative3 = derivative2.derivative()
-    jerk = np.array([derivative3.px.eval(t), derivative3.py.eval(t), derivative3.pz.eval(t)])
+    result.jerk = np.array([derivative3.px.eval(t), derivative3.py.eval(t), derivative3.pz.eval(t)])
+    
+    # 4th derivative
+    derivative4 = derivative3.derivative()
+    result.snap = np.array([derivative4.px.eval(t), derivative4.py.eval(t), derivative4.pz.eval(t)])
 
     thrust = result.acc + np.array([0, 0, 9.81]) # add gravity
 
@@ -96,8 +102,8 @@ class Polynomial4D:
     y_body = normalize(np.cross(z_body, x_world))
     x_body = np.cross(y_body, z_body)
 
-    jerk_orth_zbody = jerk - (np.dot(jerk, z_body) * z_body)
-    h_w = jerk_orth_zbody / np.linalg.norm(thrust)
+    jerk_orth_zbody = result.jerk - (np.dot(result.jerk, z_body) * z_body)
+    h_w = jerk_orth_zbody / norm(thrust)
 
     result.omega = np.array([-np.dot(h_w, y_body), np.dot(h_w, x_body), z_body[2] * dyaw])
 
@@ -107,6 +113,99 @@ class Polynomial4D:
 
     return result
 
+
+  # Matthias Faessler, Antonio Franchi, and Davide Scaramuzza:
+  #     Differential Flatness of Quadrotor Dynamics Subject to Rotor Drag for Accurate Tracking of High-Speed Trajectories.
+
+  def eval1(self, mass, t):
+    result = TrajectoryOutput()
+    # flat variables
+    result.pos = np.array([self.px.eval(t), self.py.eval(t), self.pz.eval(t)])
+    result.yaw = self.pyaw.eval(t)
+
+    # 1st derivative
+    derivative = self.derivative()
+    result.vel = np.array([derivative.px.eval(t), derivative.py.eval(t), derivative.pz.eval(t)])
+    dyaw = derivative.pyaw.eval(t)
+
+    # 2nd derivative
+    derivative2 = derivative.derivative()
+    result.acc = np.array([derivative2.px.eval(t), derivative2.py.eval(t), derivative2.pz.eval(t)])
+
+    # 3rd derivative
+    derivative3 = derivative2.derivative()
+    result.jerk = np.array([derivative3.px.eval(t), derivative3.py.eval(t), derivative3.pz.eval(t)])
+    
+    # 4th derivative
+    derivative4 = derivative3.derivative()
+    result.snap = np.array([derivative4.px.eval(t), derivative4.py.eval(t), derivative4.pz.eval(t)])
+    
+    x_c = np.array([np.cos(result.yaw), np.sin(result.yaw), 0]) 
+    y_c = np.array([-np.sin(result.yaw), np.cos(result.yaw), 0]) 
+
+    alpha = result.acc + np.array([0,0,9.81])
+    beta  = alpha
+    x_body = normalize(np.cross(y_c, alpha))
+    y_body = normalize(np.cross(beta,  x_body))
+    z_body = np.cross(x_body, y_body)
+    # compute required roll/pitch angles
+    result.pitch = np.arcsin(-x_body[2])
+    result.roll = np.arctan2(y_body[2], z_body[2])
+
+    c = np.dot(z_body, alpha)
+    
+    wx  = (-c * norm(np.cross(y_c, z_body)) * np.dot(y_body, result.jerk)) / (c * (c * norm(np.cross(y_c, z_body))))
+    wy  = (np.dot(x_body, result.jerk))/(c)
+    wz  = ((c * dyaw * np.dot(x_c, x_body)) + (np.dot(y_c, z_body) * np.dot(x_body, result.jerk))) / (c * norm(np.cross(y_c, z_body)))
+    
+    result.omega = np.array([wx, wy, wz])
+
+    return result
+
+  # A Comparative Study of Nonlinear MPC and Differential-Flatness-Based Control for Quadrotor Agile Flight
+  # Sihao Sun, Angel Romero, Philipp Foehn, Elia Kaufmann and Davide Scaramuzza
+  def eval2(self, mass, t):
+    result = TrajectoryOutput()
+    # flat variables
+    result.pos = np.array([self.px.eval(t), self.py.eval(t), self.pz.eval(t)])
+    result.yaw = self.pyaw.eval(t)
+
+    # 1st derivative
+    derivative = self.derivative()
+    result.vel = np.array([derivative.px.eval(t), derivative.py.eval(t), derivative.pz.eval(t)])
+    dyaw = derivative.pyaw.eval(t)
+
+    # 2nd derivative
+    derivative2 = derivative.derivative()
+    result.acc = np.array([derivative2.px.eval(t), derivative2.py.eval(t), derivative2.pz.eval(t)])
+
+    # 3rd derivative
+    derivative3 = derivative2.derivative()
+    result.jerk = np.array([derivative3.px.eval(t), derivative3.py.eval(t), derivative3.pz.eval(t)])
+    
+    # 4th derivative
+    derivative4 = derivative3.derivative()
+    result.snap = np.array([derivative4.px.eval(t), derivative4.py.eval(t), derivative4.pz.eval(t)])
+
+    F = mass * (result.acc + np.array([0,0,9.81]))
+   
+    z_body = normalize(F)
+    x_c = np.array([np.cos(result.yaw), np.sin(result.yaw), 0]) 
+    y_body = normalize(np.cross(z_body, x_c))
+    x_body = np.cross(y_body, z_body)
+    z_world = np.array([0,0,1])
+     
+    # compute required roll/pitch angles
+    result.pitch = np.arcsin(-x_body[2])
+    result.roll = np.arctan2(y_body[2], z_body[2])
+
+    thrust = norm(F)
+    thrustDot = np.dot(mass*result.jerk, z_body)
+    h_w = (mass/thrust)*(result.jerk - thrustDot*z_body)
+
+    result.omega = np.array([-np.dot(h_w, y_body), np.dot(h_w, x_body), dyaw*np.dot(z_world, z_body)])
+    
+    return result
 
 class Trajectory:
   def __init__(self):
@@ -127,7 +226,10 @@ class Trajectory:
       data[i,17:25] = p.pz.p
       data[i,25:33] = p.pyaw.p
     np.savetxt(filename, data, fmt="%.6f", delimiter=",", header="duration,x^0,x^1,x^2,x^3,x^4,x^5,x^6,x^7,y^0,y^1,y^2,y^3,y^4,y^5,y^6,y^7,z^0,z^1,z^2,z^3,z^4,z^5,z^6,z^7,yaw^0,yaw^1,yaw^2,yaw^3,yaw^4,yaw^5,yaw^6,yaw^7")
-
+  
+  def addmass(self, mass):
+    self.mass = mass
+  
   def stretchtime(self, factor):
     for p in self.polynomials:
       p.stretchtime(factor)
@@ -142,3 +244,28 @@ class Trajectory:
       if t < current_t + p.duration:
         return p.eval(t - current_t)
       current_t = current_t + p.duration
+
+  # Matthias Faessler, Antonio Franchi, and Davide Scaramuzza:
+  #     Differential Flatness of Quadrotor Dynamics Subject to Rotor Drag for Accurate Tracking of High-Speed Trajectories.
+  def eval1(self, t):
+    assert t >= 0
+    assert t <= self.duration
+
+    current_t = 0.0
+    for p in self.polynomials:
+      if t < current_t + p.duration:
+        return p.eval1(self.mass, t - current_t)
+      current_t = current_t + p.duration
+
+  # A Comparative Study of Nonlinear MPC and Differential-Flatness-Based Control for Quadrotor Agile Flight
+  # Sihao Sun, Angel Romero, Philipp Foehn, Elia Kaufmann and Davide Scaramuzza
+  def eval2(self, t):
+    assert t >= 0
+    assert t <= self.duration
+
+    current_t = 0.0
+    for p in self.polynomials:
+      if t < current_t + p.duration:
+        return p.eval2(self.mass, t - current_t)
+      current_t = current_t + p.duration
+
